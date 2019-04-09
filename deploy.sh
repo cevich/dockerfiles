aedbf98b46ee2096dd1db35986b7c2a5892d21ba
Chris Evich <cevich@redhat.com>
2019-04-09 10:40:56 -0400

    remove legacy deployment triggering
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,3 @@ cd "$(realpath $(dirname $0))"
 source ./lib.sh
 
 [[ "$TRAVIS_BRANCH" == "master" ]] || exit 0  # Not running against merged code
-
-[[ -n "$TOKEN" ]] || exit 0  # nothing to do
-
-curl -H "Content-Type: application/json" \
-     --data '{"docker_tag": "master"}' \
-     -X POST \
-     "https://registry.hub.docker.com/u/cevich/${FQIN_PFX}_${FQIN_SFX}/trigger/$TOKEN/"
